0aa2d1dd9c006b1366b3baac4c23d407349058e2
Viet Anh Le <mc.vietanh@gmail.com>
2022-07-13 09:20:24 -0400

    done display ui

--- src/components/Table/Row.tsx
+++ src/components/Table/Row.tsx
@@ -23,9 +23,6 @@ const Row: FC<IProduct> = ({
       <td>{categories}</td>
       {/* isCompleted */}
       <td>
-        <input type='checkbox' value={id} />
-      </td>
-      <td>
         <button
           type='button'
           className='inline-flex items-center justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:w-auto'
